d4abb518f8a9887bd81a0e7a7e3a889b87f82233
suraj Yadav <surajyadav.sy@outlook.com>
2022-11-02 13:16:33 +0530

    fixing_the_buggy_code

--- prime.js
+++ prime.js
@@ -2,14 +2,14 @@
 function prime (num){
     var count=0;
     for (let i=1;i<=num;i++){
-        if(num%2==0){
+        if(num%i==0){
             count++
         }
     }
     if(count==2){
-        console.log (true)
+        console.log (i,'is a prime')
     }else{
-        console.log(false)
+        console.log(i,"is not a prime")
     }
 }
 for (let i=1;i<=13;i++){
